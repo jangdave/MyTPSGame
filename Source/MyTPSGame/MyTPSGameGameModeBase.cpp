dcaf86a091e48cfffd215bfac4646e311aa43ebe
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2023-01-10 15:41:38 +0900

    Initial commit

---
+++ Source/MyTPSGame/MyTPSGameGameModeBase.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "MyTPSGameGameModeBase.h"
+#include "MyTPSGame.h"
+
+AMyTPSGameGameModeBase::AMyTPSGameGameModeBase()
+{
+	//로그 출력
+	//UE_LOG(LogTemp, Warning, TEXT("안녕"));
+	//UE_LOG(LogTemp, Warning, TEXT("%s"), *CALLINFO);
+	//PRINT_CALLINFO();
+
+	PRINT_LOG(TEXT("%s %s"), TEXT("Hello"), TEXT("World"));
+}
